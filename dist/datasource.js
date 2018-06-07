c4eadf8f051b35c199e6599ae0f600a97e3f2c88
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2018-06-07 21:18:27 +0900

    build

--- dist/datasource.js
+++ dist/datasource.js
@@ -125,7 +125,7 @@ System.register(['lodash', 'app/core/table_model'], function (_export, _context)
                 datasourceId: _this2.id,
                 queryType: 'timeSeriesQuery',
                 format: target.type || 'timeserie',
-                region: target.region || _this2.defaultRegion,
+                region: _this2.templateSrv.replace(target.region, options.scopedVars) || _this2.defaultRegion,
                 timestampColumn: target.timestampColumn,
                 valueColumn: target.valueColumn,
                 legendFormat: target.legendFormat || '',
