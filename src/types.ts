c7b8a6904adb1b56f58b9f3510a8a132728d7e4c
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2020-06-15 16:23:46 +0900

    migrate to react frontend

--- src/types.ts
+++ src/types.ts
@@ -2,13 +2,27 @@ import { DataQuery, DataSourceJsonData } from '@grafana/data';
 
 export interface AwsAthenaOptions extends DataSourceJsonData {
   defaultRegion: string;
+  profile: string;
+  assumeRoleArn: string;
+}
+
+export interface AwsAthenaSecureJsonData {
+  accessKey: string;
+  secretKey: string;
+}
+
+export enum AwsAuthType {
+  KEY = 'keys',
+  CREDENTIALS = 'credentials',
+  ARN = 'arn',
 }
 
 export interface AwsAthenaQuery extends DataQuery {
   refId: string;
-  format: 'timeseries' | 'table';
+  format: string;
   region: string;
   queryExecutionId: string;
+  inputs: any;
   timestampColumn: string;
   valueColumn: string;
   legendFormat: string;
